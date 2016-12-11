ae291864780392f0c562d689d2875f446a50b448
Amy Briggs <amylbd@gmail.com>
2016-12-11 16:37:39 -0700

    added guitar and bass sound files

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -41,6 +41,82 @@ app.controller('BuildController', ['$scope', function($scope){
       B: new Howl({
         urls: ['sounds/piano/acoustic_grand_piano-mp3/B4.mp3'],
       })
+    },
+    guitar: {
+      C: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/C4.mp3'],
+      }),
+      Cs: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Db4.mp3'],
+      }),
+      D: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/D4.mp3'],
+      }),
+      Eb: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Eb4.mp3'],
+      }),
+      E: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/E4.mp3'],
+      }),
+      F: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/F4.mp3'],
+      }),
+      Fs: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Gb4.mp3'],
+      }),
+      G: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/G4.mp3'],
+      }),
+      Ab: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Ab4.mp3'],
+      }),
+      A: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/A4.mp3'],
+      }),
+      Bb: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Bb4.mp3'],
+      }),
+      B: new Howl({
+        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/B4.mp3'],
+      })
+    },
+    bass: {
+      C: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/C2.mp3'],
+      }),
+      Cs: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/Db2.mp3'],
+      }),
+      D: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/D2.mp3'],
+      }),
+      Eb: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/Eb2.mp3'],
+      }),
+      E: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/E2.mp3'],
+      }),
+      F: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/F2.mp3'],
+      }),
+      Fs: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/Gb2.mp3'],
+      }),
+      G: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/G2.mp3'],
+      }),
+      Ab: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/Ab2.mp3'],
+      }),
+      A: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/A2.mp3'],
+      }),
+      Bb: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/Bb2.mp3'],
+      }),
+      B: new Howl({
+        urls: ['sounds/bass/acoustic_bass-mp3/B2.mp3'],
+      })
     }
 
   }
