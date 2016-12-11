93d24b5d4876054ac2f9991055d13597d7755b88
Amy Briggs <amylbd@gmail.com>
2016-12-11 15:46:18 -0700

    added some build controller logic

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,12 +1,18 @@
 app.controller('BuildController', ['$scope', function($scope){
   $scope.notes = false;
   $scope.drums = false;
-  $scope.notesOn = function(){
-    $scope.notes = true;
-    $scope.drums = false;
+  $scope.activeInst = [];
+
+  $scope.notesOn = function(instr){
+    if(instr == 'drums'){
+      $scope.drums = true
+      $scope.notes = false
+    } else {
+      $scope.notes = true;
+      $scope.drums = false;
+      $scope.activeInst[0] = instr
+    }
   }
-  $scope.drumsOn = function(){
-    $scope.drums = true;
-    $scope.notes = false;
-  }
+
+
 }])
