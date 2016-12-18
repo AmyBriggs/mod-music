3d92f48d54b7fb7a9d9e55031240e4948d710427
Amy Briggs <amylbd@gmail.com>
2016-12-18 15:00:11 -0700

    added a few harmonic presets

--- public/js/sounds.js
+++ public/js/sounds.js
@@ -1,1 +1,52 @@
+let cMajChords = {
+  'cMaj': ['sound0', 'sound4', 'sound7'],
+  'cMaj1': ['sound4', 'sound7', 'sound12'],
+  'cMaj2': ['sound7', 'sound12', 'sound16'],
+  'dmin': ['sound2', 'sound5', 'sound9'],
+  'dmin1': ['sound5', 'sound9', 'sound14'],
+  'emin': ['sound4', 'sound7', 'sound11'],
+  'emin1': ['sound7', 'sound11', 'sound16'],
+  'fMaj': ['sound5', 'sound9', 'sound12'],
+  'fMaj2': ['sound0', 'sound5', 'sound9'],
+  'gMaj': ['sound7', 'sound11', 'sound14'],
+  'gMaj2': ['sound2', 'sound7', 'sound11'],
+  'gMaj7': ['sound2', 'sound5', 'sound7', 'sound11'],
+  'amin': ['sound9', 'sound12', 'sound16'],
+  'amin1': ['sound0', 'sound4', 'sound9'],
+  'amin2': ['sound4', 'sound9', 'sound12']
+}
 
+let gMajChords = {
+  'gMaj': ['sound7', 'sound11', 'sound14'],
+  'gMaj2': ['sound2', 'sound7', 'sound11'],
+  'amin': ['sound9', 'sound12', 'sound16'],
+  'amin1': ['sound0', 'sound4', 'sound9'],
+  'amin2': ['sound4', 'sound9', 'sound12'],
+  'bmin1': ['sound2', 'sound6', 'sound11'],
+  'bmin2': ['sound6', 'sound11', 'sound14'],
+  'cMaj': ['sound0', 'sound4', 'sound7'],
+  'cMaj1': ['sound4', 'sound7', 'sound12'],
+  'cMaj2': ['sound7', 'sound12', 'sound16'],
+  'dMaj': ['sound2', 'sound6', 'sound9'],
+  'dMaj1': ['sound6', 'sound9', 'sound14'],
+  'dMaj7': ['sound2', 'sound6', 'sound9', 'sound12'],
+  'emin': ['sound4', 'sound7', 'sound11'],
+  'emin1': ['sound7', 'sound11', 'sound16']
+}
+
+let dMajChords = {
+  'dMaj': ['sound2', 'sound6', 'sound9'],
+  'dMaj1': ['sound6', 'sound9', 'sound14'],
+  'emin': ['sound4', 'sound7', 'sound11'],
+  'emin1': ['sound7', 'sound11', 'sound16'],
+  'gbmin': ['sound6', 'sound9', 'sound13'],
+  'gbmin2': ['sound1', 'sound6', 'sound9'],
+  'gMaj': ['sound7', 'sound11', 'sound14'],
+  'gMaj2': ['sound2', 'sound7', 'sound11'],
+  'aMaj': ['sound9', 'sound13', 'sound16'],
+  'aMaj1': ['sound1', 'sound4', 'sound9'],
+  'aMaj2': ['sound4', 'sound9', 'sound13'],
+  'aMaj7': ['sound4', 'sound7', 'sound9', 'sound13']
+  'bmin1': ['sound2', 'sound6', 'sound11'],
+  'bmin2': ['sound6', 'sound11', 'sound14'],
+}
