d30671f4489a594bbc0f4752898d97ac85972a95
Lisa Ma <limawebdev@gmail.com>
2016-12-19 11:36:04 -0700

    drum rack sounds are populated with logic

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,193 +1,242 @@
-app.controller('BuildController', ['$scope', function($scope){
-  $scope.notes = false;
-  $scope.drums = false;
-  $scope.activeInst = [];
+app.controller('BuildController', ['$scope', function($scope) {
+ $scope.notes = false;
+ $scope.drums = false;
+ $scope.bd = false; $scope.cp = false; $scope.cr = false; $scope.hh = false; $scope.ht = false; $scope.lt = false; $scope.mt = false; $scope.oh = false; $scope.rd = false; $scope.sd = false;
+ $scope.activeInst = [];
 
-  var sounds = {
-    piano: {
-      C: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/C4.mp3'],
-      }),
-      Cs: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/Db4.mp3'],
-      }),
-      D: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/D4.mp3'],
-      }),
-      Eb: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/Eb4.mp3'],
-      }),
-      E: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/E4.mp3'],
-      }),
-      F: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/F4.mp3'],
-      }),
-      Fs: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/Gb4.mp3'],
-      }),
-      G: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/G4.mp3'],
-      }),
-      Ab: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/Ab4.mp3'],
-      }),
-      A: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/A4.mp3'],
-      }),
-      Bb: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/Bb4.mp3'],
-      }),
-      B: new Howl({
-        urls: ['sounds/piano/acoustic_grand_piano-mp3/B4.mp3'],
-      })
-    },
-    guitar: {
-      C: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/C4.mp3'],
-      }),
-      Cs: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Db4.mp3'],
-      }),
-      D: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/D4.mp3'],
-      }),
-      Eb: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Eb4.mp3'],
-      }),
-      E: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/E4.mp3'],
-      }),
-      F: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/F4.mp3'],
-      }),
-      Fs: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Gb4.mp3'],
-      }),
-      G: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/G4.mp3'],
-      }),
-      Ab: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Ab4.mp3'],
-      }),
-      A: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/A4.mp3'],
-      }),
-      Bb: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Bb4.mp3'],
-      }),
-      B: new Howl({
-        urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/B4.mp3'],
-      })
-    },
-    bass: {
-      C: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/C2.mp3'],
-      }),
-      Cs: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/Db2.mp3'],
-      }),
-      D: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/D2.mp3'],
-      }),
-      Eb: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/Eb2.mp3'],
-      }),
-      E: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/E2.mp3'],
-      }),
-      F: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/F2.mp3'],
-      }),
-      Fs: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/Gb2.mp3'],
-      }),
-      G: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/G2.mp3'],
-      }),
-      Ab: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/Ab2.mp3'],
-      }),
-      A: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/A2.mp3'],
-      }),
-      Bb: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/Bb2.mp3'],
-      }),
-      B: new Howl({
-        urls: ['sounds/bass/acoustic_bass-mp3/B2.mp3'],
-      })
-    },
-    drums: {
-      bd: new Howl({
-        urls: ['sounds/drums/909/bd02.wav']
-      }),
-      cp: new Howl({
-        urls: ['sounds/drums/909/cp01.wav']
-      }),
-      cr: new Howl({
-        urls: ['sounds/drums/909/cr02.wav']
-      }),
-      hh: new Howl({
-        urls: ['sounds/drums/909/hh01.wav']
-      }),
-      ht: new Howl({
-        urls: ['sounds/drums/909/ht01.wav']
-      }),
-      lt: new Howl({
-        urls: ['sounds/drums/909/lt01.wav']
-      }),
-      mt: new Howl({
-        urls: ['sounds/drums/909/mt01.wav']
-      }),
-      oh: new Howl({
-        urls: ['sounds/drums/909/oh01.wav']
-      }),
-      rd: new Howl({
-        urls: ['sounds/drums/909/rd01.wav']
-      }),
-      sd: new Howl({
-        urls: ['sounds/drums/909/sd01.wav']
-      })
-    }
+ var sounds = {
+  piano: {
+   C: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/C4.mp3'],
+   }),
+   Cs: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/Db4.mp3'],
+   }),
+   D: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/D4.mp3'],
+   }),
+   Eb: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/Eb4.mp3'],
+   }),
+   E: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/E4.mp3'],
+   }),
+   F: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/F4.mp3'],
+   }),
+   Fs: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/Gb4.mp3'],
+   }),
+   G: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/G4.mp3'],
+   }),
+   Ab: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/Ab4.mp3'],
+   }),
+   A: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/A4.mp3'],
+   }),
+   Bb: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/Bb4.mp3'],
+   }),
+   B: new Howl({
+    urls: ['sounds/piano/acoustic_grand_piano-mp3/B4.mp3'],
+   })
+  },
+  guitar: {
+   C: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/C4.mp3'],
+   }),
+   Cs: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Db4.mp3'],
+   }),
+   D: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/D4.mp3'],
+   }),
+   Eb: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Eb4.mp3'],
+   }),
+   E: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/E4.mp3'],
+   }),
+   F: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/F4.mp3'],
+   }),
+   Fs: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Gb4.mp3'],
+   }),
+   G: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/G4.mp3'],
+   }),
+   Ab: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Ab4.mp3'],
+   }),
+   A: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/A4.mp3'],
+   }),
+   Bb: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/Bb4.mp3'],
+   }),
+   B: new Howl({
+    urls: ['sounds/guitar/acoustic_guitar_nylon-mp3/B4.mp3'],
+   })
+  },
+  bass: {
+   C: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/C2.mp3'],
+   }),
+   Cs: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/Db2.mp3'],
+   }),
+   D: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/D2.mp3'],
+   }),
+   Eb: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/Eb2.mp3'],
+   }),
+   E: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/E2.mp3'],
+   }),
+   F: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/F2.mp3'],
+   }),
+   Fs: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/Gb2.mp3'],
+   }),
+   G: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/G2.mp3'],
+   }),
+   Ab: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/Ab2.mp3'],
+   }),
+   A: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/A2.mp3'],
+   }),
+   Bb: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/Bb2.mp3'],
+   }),
+   B: new Howl({
+    urls: ['sounds/bass/acoustic_bass-mp3/B2.mp3'],
+   })
+  },
+  drums: {
+   bd: new Howl({
+    urls: ['sounds/drums/909/bd02.wav']
+   }),
+   cp: new Howl({
+    urls: ['sounds/drums/909/cp01.wav']
+   }),
+   cr: new Howl({
+    urls: ['sounds/drums/909/cr02.wav']
+   }),
+   hh: new Howl({
+    urls: ['sounds/drums/909/hh01.wav']
+   }),
+   ht: new Howl({
+    urls: ['sounds/drums/909/ht01.wav']
+   }),
+   lt: new Howl({
+    urls: ['sounds/drums/909/lt01.wav']
+   }),
+   mt: new Howl({
+    urls: ['sounds/drums/909/mt01.wav']
+   }),
+   oh: new Howl({
+    urls: ['sounds/drums/909/oh01.wav']
+   }),
+   rd: new Howl({
+    urls: ['sounds/drums/909/rd01.wav']
+   }),
+   sd: new Howl({
+    urls: ['sounds/drums/909/sd01.wav']
+   })
+  }
+ }
+
+ $scope.notesOn = function(instr) {
+  if (instr == 'drums') {
+   $scope.drums = true
+   $scope.notes = false
+  } else {
+   $scope.notes = true;
+   $scope.drums = false;
+   $scope.activeInst[0] = instr
+   $scope.instrument = instr
+   console.log();
+   console.log('instrument is', instr);
+  }
+ }
+
+ $scope.isActive = false
+ $scope.attachInstr = function() {
+  $scope.isActive = !$scope.isActive
+  if (!$scope.instrument) {
+   console.log($scope.isActive);
+   console.log('Please select an instrument');
+  } else {
+   let instr = $scope.instrument
+   $scope.instr = {
+    show: true,
+    hide: false
+   }
+   $scope.selected = instr
+   console.log('you clicked', instr);
 
   }
+ }
 
-  $scope.notesOn = function(instr){
-    if(instr == 'drums'){
-      $scope.drums = true
-      $scope.notes = false
-    } else {
-      $scope.notes = true;
-      $scope.drums = false;
-      $scope.activeInst[0] = instr
-      $scope.instrument = instr
-      console.log();
-      console.log('instrument is', instr);
-    }
+ $scope.playNote = function(note) {
+  sounds[$scope.instrument][note].play()
+ }
+ 
+ //play the drum sound
+ $scope.playDrum = function(part) {
+  sounds.drums[part].play()
+ }
+
+ //Logic for showing a panel when a certain part is clicked on (handling the ng-show logic)
+ $scope.activeDrum = function(part) {
+  switch (part) {
+   case 'bd':
+    $scope.bd = true;
+    $scope.cp = false; $scope.cr = false; $scope.hh = false; $scope.ht = false; $scope.lt = false; $scope.mt = false; $scope.oh = false; $scope.rd = false; $scope.sd = false;
+    break;
+   case 'cp':
+    $scope.cp = true;
+    $scope.bd = false; $scope.cr = false; $scope.hh = false; $scope.ht = false; $scope.lt = false; $scope.mt = false; $scope.oh = false; $scope.rd = false; $scope.sd = false;
+    break;
+   case 'cr':
+    $scope.cr = true;
+    $scope.bd = false; $scope.cp = false; $scope.hh = false; $scope.ht = false; $scope.lt = false; $scope.mt = false; $scope.oh = false; $scope.rd = false; $scope.sd = false;
+    break;
+   case 'hh':
+    $scope.hh = true;
+    $scope.bd = false; $scope.cp = false; $scope.cr = false; $scope.ht = false; $scope.lt = false; $scope.mt = false; $scope.oh = false; $scope.rd = false; $scope.sd = false;
+    break;
+   case 'ht':
+    $scope.ht = true;
+    $scope.bd = false; $scope.cp = false; $scope.cr = false; $scope.hh = false; $scope.lt = false; $scope.mt = false; $scope.oh = false; $scope.rd = false; $scope.sd = false;
+    break;
+   case 'lt':
+    $scope.lt = true;
+    $scope.bd = false; $scope.cp = false; $scope.cr = false; $scope.hh = false; $scope.ht = false; $scope.mt = false; $scope.oh = false; $scope.rd = false; $scope.sd = false;
+    break;
+   case 'mt':
+    $scope.mt = true;
+    $scope.bd = false; $scope.cp = false; $scope.cr = false; $scope.hh = false; $scope.ht = false; $scope.lt = false; $scope.oh = false; $scope.rd = false; $scope.sd = false;
+    break;
+   case 'oh':
+    $scope.oh = true;
+    $scope.bd = false; $scope.cp = false; $scope.cr = false; $scope.hh = false; $scope.ht = false; $scope.lt = false; $scope.mt = false; $scope.rd = false; $scope.sd = false;
+    break;
+   case 'rd':
+    $scope.rd = true;
+    $scope.bd = false; $scope.cp = false; $scope.cr = false; $scope.hh = false; $scope.ht = false; $scope.lt = false; $scope.mt = false; $scope.oh = false; $scope.sd = false;
+    break;
+   case 'sd':
+    $scope.sd = true;
+    $scope.bd = false; $scope.cp = false; $scope.cr = false; $scope.hh = false; $scope.ht = false; $scope.lt = false; $scope.mt = false; $scope.oh = false; $scope.rd = false;
+    break;
   }
-
-  $scope.isActive = false
-  $scope.attachInstr = function(){
-    $scope.isActive = !$scope.isActive
-    if(!$scope.instrument){
-      console.log($scope.isActive);
-      console.log('Please select an instrument');
-    }
-    else {
-      let instr = $scope.instrument
-      $scope.instr = {
-        show: true,
-        hide: false
-      }
-      $scope.selected = instr
-      console.log('you clicked', instr);
-
-    }
-}
-
-  $scope.playNote = function(note){
-    sounds[$scope.instrument][note].play()
-  }
-
+ }
 }])
