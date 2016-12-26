68b6452f9cef5ee73785fa95a86b8a48e0823275
Amy Briggs <amylbd@gmail.com>
2016-12-26 16:09:48 -0700

    added a few lines of code for profile switching to branch now

--- public/js/profile_controller.js
+++ public/js/profile_controller.js
@@ -1,7 +1,10 @@
 app.controller('ProfileController', function($scope, LoginService, $location, $cookies) {
   $scope.cookies = $cookies.getAll();
   var user = angular.fromJson($scope.cookies.loggedIn);
+  console.log(user);
   $scope.user = user.username;
+  $scope.desc = user.desc
+  $scope.image = user.img;
   $scope.views = user.views;
 
 })
--- routes/profile.js
+++ routes/profile.js
@@ -3,11 +3,21 @@ var router = express.Router();
 var knex = require('../knex')
 
 /* GET users listing. */
+router.get('/', function(req, res, next) {
+  knex('users')
+  .then(function(data){
+    res.json(data)
+    console.log('users', users);
+  })
+});
+
 router.get('/:id', function(req, res, next) {
   knex('users')
-  .where('username', req.params.username)
-  console.log(req.body.username);
-  res.send('Welcome to profile');
+  .where('id', req.params.id)
+  .first()
+  .then(function(data){
+    res.json(data)
+  })
 });
 
 module.exports = router;
