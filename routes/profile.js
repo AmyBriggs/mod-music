b8629872a4f9afedd13f14181215e7f1ee0ecdbe
Amy Briggs <amylbd@gmail.com>
2016-12-28 18:21:15 -0500

    adding some basic styling to profile page

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var knex = require('../knex')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/profile', function(req, res, next) {
   knex('users')
   .then(function(users){
     res.json(users)
@@ -11,11 +11,11 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/:id', function(req, res, next){
+router.get('/profile', function(req, res, next){
   knex('users')
   .where('users.id', req.params.id)
   .join('projects', 'users.id', 'projects.user_id')
-  .select('users.id', 'users.username', 'users.views', 'users.desc', 'users.img', 'projects.instrObj', 'projects.views', 'projects.title',
+  .select('users.id', 'users.username', 'users.views', 'users.desc', 'users.img', 'projects.build', 'projects.views', 'projects.title',
   'projects.desc', 'projects.genre', 'projects.votes', 'projects.created_at', 'projects.updated_at')
   .then(function(user){
     res.json(user)
