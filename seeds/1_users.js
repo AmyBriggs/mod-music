a585cc8f89a17c502416ed96444a81784cff04f7
Amy Briggs <amylbd@gmail.com>
2016-12-31 18:28:57 -0700

    changed profile pics

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -6,13 +6,13 @@ exports.seed = function(knex) {
     hashed_pass: '$2a$08$SFfc0YbRbeA580.czP58.uaCpiiyQpIWjt58yQQOgCLmgeKgfBI4m',
     views: 500,
     desc: 'Piano woman',
-    img: 'http://furlingtonpost.com/wp-content/uploads/2015/10/Screen-shot-2015-10-21-at-8.05.38-PM.png'
+    img: 'img/Amy.jpg'
    }, {
     username: 'lisa',
     hashed_pass: '$2a$08$SFfc0YbRbeA580.czP58.uaCpiiyQpIWjt58yQQOgCLmgeKgfBI4m',
     views: 250,
     desc: 'I built this app.',
-    img: 'https://www.overthinkingit.com/wp-content/uploads/2013/10/Lemongrab.jpg'
+    img: 'img/Lisa.jpg'
    }]);
   });
 };
