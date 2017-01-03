f0e6806ec0e607355299ba09f09c0dbcfd095599
Lisa Ma <limawebdev@gmail.com>
2017-01-03 14:02:13 -0700

    seeded database with mad users and projects

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -4,15 +4,51 @@ exports.seed = function(knex) {
    return knex('users').insert([{
     username: 'amylbd',
     hashed_pass: '$2a$08$SFfc0YbRbeA580.czP58.uaCpiiyQpIWjt58yQQOgCLmgeKgfBI4m',
-    views: 500,
-    desc: 'Piano woman',
+    views: 468,
+    desc: 'Sometimes I play piano with my nose. Sometimes I build full-stack apps.',
     img: 'img/Amy.jpg'
    }, {
     username: 'lisa',
     hashed_pass: '$2a$08$SFfc0YbRbeA580.czP58.uaCpiiyQpIWjt58yQQOgCLmgeKgfBI4m',
-    views: 250,
-    desc: 'I built this app.',
+    views: 124,
+    desc: 'One time the DJ went on a 30-min bathroom break and asked me to sub in. I pressed buttons, twisted knobs, and became a DJ. Fooling everyone everyday.',
     img: 'img/Lisa.jpg'
+   }, {
+    username: 'yames',
+    hashed_pass: '$2a$08$SFfc0YbRbeA580.czP58.uaCpiiyQpIWjt58yQQOgCLmgeKgfBI4m',
+    views: 275,
+    desc: "You'll never know what I'm listening to and you don't want to know.",
+    img: 'http://orig12.deviantart.net/317d/f/2009/255/d/f/skull_and_headphones_by_hiddenmoves.jpg'
+   }, {
+    username: 'crAAg',
+    hashed_pass: '$2a$08$SFfc0YbRbeA580.czP58.uaCpiiyQpIWjt58yQQOgCLmgeKgfBI4m',
+    views: 780,
+    desc: 'My beats are probably better than your beats. Untz untz.',
+    img: 'https://s-media-cache-ak0.pinimg.com/736x/dc/c7/e1/dcc7e114efe4151a8b862086d22a8205.jpg'
+   }, {
+    username: 'bouchybanjo',
+    hashed_pass: '$2a$08$SFfc0YbRbeA580.czP58.uaCpiiyQpIWjt58yQQOgCLmgeKgfBI4m',
+    views: 916,
+    desc: 'Beautiful banjo bliss is better than bland blunt beats.',
+    img: 'https://cdn.shopify.com/s/files/1/0204/9822/products/clawgrass-banjo---neck-and-pot_1024x1024.jpeg?v=1447348916'
+   }, {
+    username: 'ekopeisdope',
+    hashed_pass: '$2a$08$SFfc0YbRbeA580.czP58.uaCpiiyQpIWjt58yQQOgCLmgeKgfBI4m',
+    views: 352,
+    desc: 'Dope beat$. Fre$h beat$.',
+    img: 'http://www.lovethispic.com/uploaded_images/31279-Sound-Board.jpg'
+   }, {
+    username: 'ziopads',
+    hashed_pass: '$2a$08$SFfc0YbRbeA580.czP58.uaCpiiyQpIWjt58yQQOgCLmgeKgfBI4m',
+    views: 819,
+    desc: 'I own a lot of synths and I know how to use them all.',
+    img: 'http://www.creatingtrance.com/wp-content/uploads/2014/04/giant-modular-synth.jpg'
+   },{
+    username: 'decafgrandeoxytank',
+    hashed_pass: '$2a$08$SFfc0YbRbeA580.czP58.uaCpiiyQpIWjt58yQQOgCLmgeKgfBI4m',
+    views: 700,
+    desc: 'Inhale oxygen. Exhale beats.',
+    img: 'http://www.harborfreight.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/m/image_11359.jpg'
    }]);
   });
 };
--- seeds/2_projects.js
+++ seeds/2_projects.js
@@ -4,31 +4,122 @@ exports.seed = function(knex) {
    return knex('projects').insert([{
     user_id: 1,
     views: 125,
-    title: 'My Project 1',
+    title: 'grass is blue',
     desc: 'My first project ever. Rad.',
-    genre: 'Bluegrass',
-    votes: 798
+    genre: 'Alternative',
+    votes: 111
    }, {
     user_id: 1,
-    views: 126,
-    title: 'My Project 2',
+    views: 146,
+    title: 'comfort zone',
     desc: 'My second project ever. Double Rad.',
-    genre: 'R&B',
-    votes: 799
+    genre: 'Classical',
+    votes: 833
    }, {
     user_id: 2,
-    views: 121,
-    title: 'My Project 1',
-    desc: 'My first project ever. Double Rad.',
+    views: 144,
+    title: 'untz untz',
+    desc: 'only drum and bass. deals with it.',
     genre: 'Electronic',
-    votes: 799
+    votes: 188
    }, {
     user_id: 2,
-    views: 122,
-    title: 'My Project 2',
-    desc: 'My second project ever. Rad.',
+    views: 524,
+    title: "destiny's parent",
+    desc: "destiny's child's grandparent",
     genre: 'R&B',
-    votes: 799
-   }]);
+    votes: 249
+   }, {
+    user_id: 3,
+    views: 526,
+    title: 'silence',
+    desc: 'all the instruments, no notes. so deep.',
+    genre: 'Alternative',
+    votes: 142
+   }, {
+    user_id: 3,
+    views: 888,
+    title: "turn uppp",
+    desc: "the opposite of silence",
+    genre: 'Pop',
+    votes: 145
+   }, {
+    user_id: 3,
+    views: 527,
+    title: 'aerosmith clone',
+    desc: "i don't want to miss a thing",
+    genre: 'Rock',
+    votes: 143
+   }, {
+    user_id: 4,
+    views: 900,
+    title: "classy crAAg",
+    desc: "a classical piece by classy ol' me",
+    genre: 'Classical',
+    votes: 838
+   }, {
+    user_id: 4,
+    views: 723,
+    title: 'i will rock you',
+    desc: 'i will.',
+    genre: 'Rock',
+    votes: 672
+   }, {
+    user_id: 5,
+    views: 524,
+    title: "big imposter banjo",
+    desc: "still a string instrument",
+    genre: 'Alternative',
+    votes: 123
+   }, {
+    user_id: 5,
+    views: 82,
+    title: 'oh no',
+    desc: 'i think i might be leaving the banjo for these sick beatz',
+    genre: 'Electronic',
+    votes: 521
+   }, {
+    user_id: 5,
+    views: 124,
+    title: "toxic",
+    desc: "britney spears acoustic",
+    genre: 'Pop',
+    votes: 774
+   }, {
+    user_id: 6,
+    views: 523,
+    title: 'fre$h',
+    desc: 'so fre$h. so clean.',
+    genre: 'Electronic',
+    votes: 413
+   }, {
+    user_id: 6,
+    views: 435,
+    title: "smoove",
+    desc: "smoove just like me",
+    genre: 'R&B',
+    votes: 832
+   }, {
+    user_id: 7,
+    views: 152,
+    title: 'polyrhythmic',
+    desc: 'is this app polyrhythmic?',
+    genre: 'Classical',
+    votes: 142
+   }, {
+    user_id: 8,
+    views: 744,
+    title: "Rev 22:20",
+    desc: "praise be",
+    genre: 'Rock',
+    votes: 264
+   },{
+    user_id: 8,
+    views: 914,
+    title: "and she said...",
+    desc: "how did we get ourselves so lost",
+    genre: 'Pop',
+    votes: 243
+   },]);
   });
 };
