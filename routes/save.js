8791dc0c82791d1abbae7fd97cc6de1bd92cb2d1
Lisa Ma <limawebdev@gmail.com>
2017-01-03 00:29:12 -0700

    update function to edit projects working with save

--- routes/save.js
+++ routes/save.js
@@ -4,18 +4,31 @@ var knex = require('../knex')
 
 router.post('/', function(req, res, next){
   knex('projects')
-    .returning('*')
-    .insert({
-      user_id: req.body.id,
-      build: req.body.build,
-      views: 0,
-      title: req.body.projTitle,
-      desc: req.body.desc,
-      genre: req.body.genre,
-      votes: 0
-    }).then(project => {
-      console.log(project);
+    .where('title', req.body.projTitle)
+    .then((project) => {
+      if(project.length == 0){
+        knex('projects')
+        .returning('*')
+        .insert({
+          user_id: req.body.id,
+          build: req.body.build,
+          views: 0,
+          title: req.body.projTitle,
+          desc: req.body.desc,
+          genre: req.body.genre,
+          votes: 0
+        })
+      } else{
+        knex('projects')
+          .where('title', req.body.projTitle)
+          .update({
+            build: req.body.build,
+            desc: req.body.desc,
+            genre: req.body.genre
+          })
+      }
     })
+
 })
 
 
